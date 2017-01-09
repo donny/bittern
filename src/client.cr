cd8464e7f37ef13df32544c5bebeecfe70e4f4a5
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-01-09 13:31:20 +1100

    Implement the real client server code

--- src/client.cr
+++ src/client.cr
@@ -1,10 +1,39 @@
 module Bittern
   class Client
+    @host : String
+    @port : Int32
+
     def initialize(@option : Option)
+      @host = @option.server_address
+      @port = @option.server_port.to_i
+      @socket = TCPSocket.new(@host, @port)
     end
 
     def run
       puts "Client"
+
+      process_message()
+
+      loop do
+        input = gets
+
+        if input.nil? || input == "exit"
+          break
+        else
+          input += "\n"
+          @socket.write(input.to_slice)
+        end
+      end
+      @socket.close
+    end
+
+    def process_message
+      spawn do
+        loop do
+          response = @socket.gets
+          puts response
+        end
+      end
     end
   end
 end
--- src/server.cr
+++ src/server.cr
@@ -1,10 +1,45 @@
+require "socket"
+
 module Bittern
   class Server
+    @host : String
+    @port : Int32
+    @connected_clients = [] of TCPSocket
+
     def initialize(@option : Option)
+      @host = @option.server_address
+      @port = @option.server_port.to_i
+      @server = TCPServer.new(@host, @port)
     end
 
     def run
       puts "Server"
+      loop do
+        socket = @server.accept
+        @connected_clients.push(socket)
+        process_connection(socket)
+      end
+    end
+
+    def process_connection(socket)
+      spawn do
+        loop do
+          message = socket.gets
+          if message.nil?
+            socket.close
+            break
+          else
+            message += "\n"
+
+            puts message
+
+            @connected_clients.map do |client|
+              client.write(message.to_slice)
+            end
+          end
+          break if socket.closed?
+        end
+      end
     end
   end
 end
